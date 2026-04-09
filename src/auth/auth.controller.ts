be56d6adac15aa657d5434aa8f4df16c2c6ce215
Omkar Arote <devommac@Omkars-MacBook-Air.local>
2026-04-09 23:25:36 +0530

    Added protected routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 import { RegisterUserDto } from './dto/registerUser.dto';
 
 @Controller('auth')
@@ -17,5 +18,13 @@ export class AuthController {
   async register(@Body() registerDto: RegisterUserDto) {
     // Register logic here
     return await this.authService.registerUser(registerDto);
+  }
+
+  @Get('profile')
+  @UseGuards(AuthGuard)
+  async getProfile(@Request() req) {
+    // Profile logic here
+    // console.log('Profile route accessed', req.user); // This will log the user information extracted from the JWT token
+    return req.user; // This will return the user information extracted from the JWT token
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { ConfigModule } from '@nestjs/config';
       global: true,
       // secret: jwtConstants.secret,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '48h' },
     }),
   ],
   controllers: [AuthController],
